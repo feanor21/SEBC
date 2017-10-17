a6673212196c19aadc51c690549b2767eba054e9
feanor21 <vblondeau21@gmail.com>
2017-10-17 16:03:58 +0200

    add sh file

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -3,32 +3,36 @@
 
 MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
 HADOOP=/opt/cloudera/parcels/CDH/bin
-
+HADOOP_USER_NAME=hdfs
 # Mark start of the loop
 echo Testing loop started on `date`
 
 # Mapper containers
-for i in 8    
+for i in 8 4 2   
 do
+echo "mapper number : $i"
    # Reducer containers
-   for j in 1 
-   do                 
+   for j in 1 2 4 
+   do      
+	echo "Reducer number : $j"
       # Container memory
-      for k in 512 1024 
+      for k in 512 1024 2048 
       do                         
          # Set mapper JVM heap 
          MAP_MB=`echo "($k*0.8)/1" | bc` 
-
+		echo "map jvm heap memory in MB; $MAP_MB"
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
-
-        time ${HADOOP}/hadoop jar ${MR}/hadoop-examples.jar teragen \
+		echo "reducer jvm heap memory in MB; $RED_MB"
+		echo "container memory: $k"
+		
+        time HADOOP_USER_NAME=hdfs ${HADOOP}/hadoop jar ${MR}/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      51200000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-       time ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
+       time HADOOP_USER_NAME=hdfs ${HADOOP}/hadoop jar $MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
@@ -38,8 +42,8 @@ do
 	             /results/tg-10GB-${i}-${j}-${k}  \
                      /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-        $HADOOP/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+        HADOOP_USER_NAME=hdfs $HADOOP/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
+        HADOOP_USER_NAME=hdfs $HADOOP/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
       done
    done
 done
